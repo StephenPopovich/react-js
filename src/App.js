41c5454388e9adb715f3e83c09de6290f6ba3bf3
StephenPopovich <stephenpopovich16@gmail.com>
2018-07-04 14:07:58 -0700

    add functional switch button

--- src/App.js
+++ src/App.js
@@ -3,14 +3,34 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      {name: 'Max', age: 28},
+      {name: 'Manu', age: 29},
+      {name: 'Stephanie', age: 26}
+    ],
+    otherState: 'Some Other Value'
+  }
+
+  switchNameHandler = () => {
+    // console.log('working!');
+    this.setState({
+      persons:[
+      {name: 'Mark', age: 31},
+      {name: 'Sarah', age: 29},
+      {name: 'Charolette', age: .5}
+    ]
+    })
+  }
   render() {
     return (
       <div className="App">
        <h1>Hi It's React  </h1>
        <p> this is really working </p>
-       <Person name="max" age="23"/>
-       <Person name="john" age="28">My Hobbies: Racing </Person>
-       <Person name="jill" age="13"/>
+       <button onClick={this.switchNameHandler}>Switch Name</button>
+       <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+       <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing </Person>
+       <Person name={this.state.persons[2].name}  age={this.state.persons[2].age}/>
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
